9639037ea8fd97530df9fa3eddaf0a9f5704c671
Mark Lee <mlee.aus@gmail.com>
2021-01-26 16:42:12 +1100

    Fixed logging as previous version was a quick draft that failed

--- gribble.ps1
+++ gribble.ps1
@@ -1,14 +1,17 @@
-# Quick and Dirty Powershell Bulk emailer
+# Gribble: Quick and Dirty Powershell Bulk emailer
+
 $csvFile = Import-Csv ".\email_addresses.csv"
 $settings = Get-Content -Raw -Path .\settings.json | ConvertFrom-Json
 
-$SMTPServer = $settings."server"
-$SMTPPort = $settings."port"
+$SMTPServer = $settings.server
+$SMTPPort = $settings.port
 
-$username = $settings."username"
-$password = ConvertTo-SecureString $settings."password" -AsPlainText -Force
 
-$logObj = New-Object -TypeName pscustomobject
+$username = $settings.username
+$password = ConvertTo-SecureString $settings.password -AsPlainText -Force
+
+$progressCounter = 0
+$totalCount = $csvFile.Count
 
 $csvFile | ForEach-Object{
    
@@ -20,16 +23,29 @@ $csvFile | ForEach-Object{
   $Body += "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do...</p>"
 
   try {
+
     Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body -BodyAsHtml -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential (New-Object System.Management.Automation.PSCredential ($username, $password))
-    Write-Host "Sent message to: $To"
-    $logObj | Add-Member -Force -MemberType NoteProperty -Name Email -Value $To
-    Write-Host "Email sent to: $_.emailaddress"  
-    $logObj | Add-Member -Force -MemberType NoteProperty -Name Status -Value 'Email sent SUCCESSFULLY'
-    Export-Csv -Path './log.csv' -InputObject $logObj -Append -NoTypeInformation
+    
+    $log = [PSCustomObject]@{
+      Status = 'Success'
+      Recipient = $To
+    }
+    
+    Write-Progress -Activity $To -Status "Success:" -PercentComplete ($progressCounter / $totalCount * 100)
+
+    Export-Csv -Path './log.csv' -InputObject $log -Append -NoTypeInformation
+
   } catch {
-    $logObj | Add-Member -Force -MemberType NoteProperty -Name Email -Value $To
-    $logObj | Add-Member -Force -MemberType NoteProperty -Name Status -Value 'Email sending FAILED'
-    Write-Host "FAILED: $_.emailaddress"
-    Export-Csv -Path './log.csv' -InputObject $logObj -Append -NoTypeInformation
+
+    $log = [PSCustomObject]@{
+      Status = 'Failure'
+      Recipient = $To
+    }
+
+    Write-Progress -Activity $To -Status "Failure:" -PercentComplete ($progressCounter / $totalCount * 100)
+
+    Export-Csv -Path './log.csv' -InputObject $log -Append -NoTypeInformation
   }
+
+  $progressCounter = $progressCounter + 1
 }
